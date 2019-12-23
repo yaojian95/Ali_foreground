1a4246103cfed4ff5a6d148792167a059ddeaf24
yaojian95 <1427456796@qq.com>
2019-12-24 04:40:35 +0800

    sum for bin_l rather than np.mean

--- codes/utils.py
+++ codes/utils.py
@@ -8,19 +8,23 @@ import numpy as np
 import healpy as hp
 
 def bin_l(cl, L, Q, l2 = False):
-    ''' cl, L, Q(bin_number)'''
+    ''' 
+    
+    cl, L, Q(bin_number); no np.mean
+    
+    '''
     if len(cl.shape) > 2 :
         bin_averages = np.zeros((Q, cl.shape[1], cl.shape[1]))
     else:
         bin_averages = np.zeros(Q)
-	if l2:
-    	for l in range(L):
-        	cl[l] = l*(l+1)/2/np.pi*(cl[l])    
+    if l2:
+        for l in range(L):
+            cl[l] = l*(l+1)/2/np.pi*(cl[l])    
     for q in range(Q):
         if q == 0:
-            bin_averages[q] = np.mean(cl[2:((q+1)*L//Q)])   ### l = 0,1 should be abandoned
+            bin_averages[q] = sum(cl[2:((q+1)*L//Q)]/(L//Q))   ### l = 0,1 should be abandoned; SHOULDN'T use np.mean(), becaues it need to specify AXIS... . But for different shape, the axis is different .
         else:
-            bin_averages[q] = np.mean(cl[q*L//Q:((q+1)*L//Q)])
+            bin_averages[q] = sum(cl[q*L//Q:((q+1)*L//Q)]/(L//Q))
     return bin_averages
 
 def get_ell(L, Q):
@@ -33,14 +37,14 @@ def get_ell(L, Q):
 def Power_spectrum(maps,R, lmax):
     global cl
     ''' 
-	revised for upper-triangle of the matrix.
-	Input:
-	maps with multi-frequencies IQU sky maps. 
-	Galactic plane cut for calculating the power spectrum.
-	lamx.  
+    revised for upper-triangle of the matrix.
+    Input:
+    maps with multi-frequencies IQU sky maps. 
+    Galactic plane cut for calculating the power spectrum.
+    lamx.  
 
     need to be generalized to E B PS simulantaneously. 
-	'''
+    '''
     n_f = len(maps)
     cl = np.ones((n_f*n_f, lmax +1)); Cl = np.zeros((lmax+1, n_f, n_f))
     k = 0
@@ -67,3 +71,18 @@ def m_l(lmax, l):
     for i in range(l + 1):
         m_id[i] = hp.sphtfunc.Alm.getidx(lmax, l , i)
     return (m_id)
+
+
+ali_ma = hp.read_map('/smc/jianyao/Ali_maps/ali_mask_wo_edge.fits')#, verbose=False)
+def Mask(maps): 
+    maps_ma = hp.ma(maps)
+    maps_ma.mask = np.logical_not(ali_ma)
+    return maps_ma
+
+def l2(ell):
+    '''
+    get the l^2/np.pi
+    '''
+    
+    return ell*(ell+1)/2/np.pi
+
